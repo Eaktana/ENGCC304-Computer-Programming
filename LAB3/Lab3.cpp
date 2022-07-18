5ee828f41145e45d1ca50dc9f43b0609ce68b26f
EaktanaO_O <108009014+Eaktana@users.noreply.github.com>
2022-07-18 19:22:40 +0700

    Add files via upload

---
+++ LAB3/Lab3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int   a = 0 ;
+    float b = 0 ;
+    char  c = 0 ;
+
+    scanf( "%d %f %c" , &a , &b , &c ) ;
+
+    printf( "Please enter value : %d %.2f %c" , a , b , c ) ;
+
+    return 0 ;
+}//end function
